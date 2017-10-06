5732ea0cbb35486111949a6598130847b1fb582d
eonezhang <eonezhang@qq.com>
2017-10-06 17:32:41 +0800

    test

---
+++ src/test/java/com/eone/MainTest.java
@@ -0,0 +1,14 @@
+package com.eone;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+    @Test
+    public void sum() throws Exception {
+        int a = 1;
+        assertEquals(1, a);
+    }
+
+}
