b962f450b15f0e89bc12419f73e44e7b4fc0125e
eonezhang <eonezhang@qq.com>
2017-10-06 17:47:16 +0800

    test

--- src/main/java/com/eone/Main.java
+++ src/main/java/com/eone/Main.java
@@ -1,12 +1,15 @@
 package com.eone;
 
 public class Main {
-  private Main() {}
-  public int sum(int a, int b) {
-  	return a + b;
-  }
-  public static void main(String[] args) {
-  	Main main = new Main();
-  	main.sum(1, 2);
-  }
+    private Main() {
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.sum(1, 2);
+    }
+
+    public int sum(int a, int b) {
+        return a + b;
+    }
 }
